23c7ffc73797f636990509353be5ab3ace3710c8
25-1823-glitch <25-1823@swjb.hs.kr>
2025-08-14 17:32:09 +0900

    Add files via upload

---
+++ ad/test01.js
@@ -0,0 +1,10 @@
+let bunho = 1;//값을 숫자로 활당 변수의 자료형 - 숫자
+
+    console.log(typeof bunho01);
+ console.log("bunho01 ==========");
+
+  //  let bunho= "1" 값을 문자로 활당 변수의 자료형 - 문자
+   console.log("bunho01 ==========");
+   colsole.log(typeof bunho02);
+
+ 
